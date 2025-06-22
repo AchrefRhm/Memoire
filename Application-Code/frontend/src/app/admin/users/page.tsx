8e5b395ce679cab0b9136ce1c9d8ee622429d3be
AchrefRhm <116440967+AchrefRhm@users.noreply.github.com>
2025-06-22 12:26:22 +0000

    Update page.tsx

--- Application-Code/frontend/src/app/admin/users/page.tsx
+++ Application-Code/frontend/src/app/admin/users/page.tsx
@@ -284,7 +284,7 @@ export default function UsersPage() {
                                   src={
                                     user.img.startsWith("https")
                                       ? user.img
-                                      : `http://localhost:5000${user.img}`
+                                      : `http://k8s-threetie-mainlb-11c5700e30-1182053200.us-east-1.elb.amazonaws.com${user.img}`
                                   }
                                   alt={user.username}
                                   className="h-10 w-10 rounded-full object-cover"

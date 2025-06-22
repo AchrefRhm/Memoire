94f56dca9002483aaeafb43b5549e12dff372185
AchrefRhm <116440967+AchrefRhm@users.noreply.github.com>
2025-06-22 12:28:04 +0000

    Update page.tsx

--- Application-Code/frontend/src/app/checkout/success/page.tsx
+++ Application-Code/frontend/src/app/checkout/success/page.tsx
@@ -103,7 +103,7 @@ const PaymentSuccessPage = () => {
 
   useEffect(() => {
     if (email && serviceId) {
-      axios.post("http://localhost:5000/api/auth/subscriptions/confirm", {
+      axios.post("http://k8s-threetie-mainlb-11c5700e30-1182053200.us-east-1.elb.amazonaws.com/api/auth/subscriptions/confirm", {
         email,
         plan: serviceId,
       });
